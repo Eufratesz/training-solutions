dcdd34149360824183a857942922ecdfe8396534
Lökösházi Anett <lokoshazi@freemail.hu>
2020-10-31 00:58:18 +0100

    Calculator - with an alternate version again

---
+++ src/main/java/classstructureio/Calculator.java
@@ -0,0 +1,28 @@
+package classstructureio;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Write a whole number, please!");
+        Scanner scanner = new Scanner(System.in);
+        int firstNumber = scanner.nextInt();
+
+        System.out.println("Write another whole number, please!");
+        int secondNumber = scanner.nextInt();
+
+        System.out.println("Let's do the addition!");
+        System.out.println(firstNumber + " + " +  secondNumber);
+        int totalAmount = firstNumber + secondNumber;
+        System.out.println(totalAmount);
+
+        /*
+        Másik megoldás az utolsó 2 sor helyett:
+        System.out.println("The total amount is: ");
+        System.out.println(firstNumber + secondNumber);
+*/
+
+
+
+    }
+}
