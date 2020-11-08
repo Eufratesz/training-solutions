d9a23c339e326bdac9d29830ddbe18e7331d7250
Lökösházi Anett <lokoshazi@freemail.hu>
2020-11-08 17:52:38 +0100

    Office.java kész, a másik kettőn még dolgozom.

---
+++ src/main/java/week02/Office.java
@@ -0,0 +1,23 @@
+package week02;
+
+import java.util.List;
+
+public class Office {
+ //   Office office = new Office;
+   // List<String> MeetingRooms =
+
+
+            /*
+            addMeetingRoom(MeetingRoom meetingRoom);
+            printNames()
+            printNamesReverse()
+            print EventNames()
+            printAreas()
+            printMeetingRoomsWithName(String name)
+            printMeetingRoomsContains (String part)
+            printAreasLargerThan (int area)
+
+            Ciklusokkal és feltételekkel dolgoznak
+             */
+
+}
