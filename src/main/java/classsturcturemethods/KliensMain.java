bfb94878586f128cfc00ab15703f1c3ab02d0b98
Lökösházi Anett <lokoshazi@freemail.hu>
2020-11-15 17:04:35 +0100

    Gyakorlásképp újra megírtam

---
+++ src/main/java/classsturcturemethods/KliensMain.java
@@ -0,0 +1,19 @@
+package classsturcturemethods;
+
+public class KliensMain {
+
+    public static void main(String[] args) {
+        Kliens kliens = new Kliens();
+
+        kliens.setName("Nagy Jolán");
+        kliens.setYear(1970);
+        kliens.setAddress("Budapest");
+
+        System.out.println("Név: " + kliens.getName());
+        System.out.println("Születési év: " + kliens.getYear());
+        System.out.println("Lakcím: " + kliens.getAddress());
+        System.out.println();
+        kliens.migrate("Szeged");
+        System.out.println("A címváltozás sikerült. Az új címe: "+ kliens.getAddress());
+    }
+}
