f7dd79193f15f28b5b6b4df41780364d3ee0ecd4
Lökösházi Anett <lokoshazi@freemail.hu>
2020-11-16 09:24:33 +0100

    Gyakorlásképp újra megoldottam.

--- src/main/java/localvariables/LocalVariablesMain.java
+++ src/main/java/localvariables/LocalVariablesMain.java
@@ -3,27 +3,30 @@ package localvariables;
 public class LocalVariablesMain {
 
     public static void main(String[] args) {
-        boolean b = false;
+        boolean b;
+       // System.out.println(b);  Nem lehet kiírni az értékét.
+
+        b = false;
         System.out.println(b);
 
+       // System.out.println(a);  definiálás előtt nem lehet kiírni.
+
         int a = 2;
-        int i = 3; int j = 4;
-
+        int i =3; int j = 4;
         int k = i;
-        System.out.println(k);
 
-        String s = "Hello World";
+        String s = "Hello Wold!";
         String t = s;
+        System.out.println(s);
         System.out.println(t);
 
-
-
-         {
-             int x = 0;
-             System.out.println(x);
-             System.out.println(a); // a blokk előtt definiált a-t kiírja, ha a blokkból íratom.
-         };
-     //   System.out.println(x); innen nem lehet, hibajel van.
-
+        {
+            int x = 0;
+            System.out.println(a);  // a blokk előtt definiált 'a' értékét.
+        }
+ //       System.out.println(x); blokkon kívül nem lehet kiírni.
     }
+
+
+
 }
