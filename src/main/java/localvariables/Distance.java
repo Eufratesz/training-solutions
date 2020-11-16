dd599ef37226e48ac0baa820fef3d4f54a2749cc
Lökösházi Anett <lokoshazi@freemail.hu>
2020-11-16 10:04:59 +0100

    Gyakorlásképp újra megoldottam

--- src/main/java/localvariables/Distance.java
+++ src/main/java/localvariables/Distance.java
@@ -1,21 +1,27 @@
 package localvariables;
 
 public class Distance {
-    private double distanceInKm;
-    private boolean exact;
 
+   private double distanceInKm;
+   private boolean exact;
 
-    public Distance(double distanceInKm, boolean exact) {
-        this.distanceInKm = distanceInKm;
-        this.exact = exact;
-    }
+   public Distance(double distanceInKm, boolean exact) {
+       this.distanceInKm = distanceInKm;
+       this.exact = exact;
+   }
 
-    public double getDistanceInKm() {
-        return distanceInKm;
-    }
+   public double getDistanceInKm() {
+       return distanceInKm;
+   }
 
-    public boolean isExact() {
-        return exact;
-    }
+   public boolean isExact() {
+       return exact;
+   }
+
 }
-//DistanceMain képmetszővel kivágva, elmentve. Hibás valami nagyon.
+
+
+
+
+
+
--- src/main/java/localvariables/DistanceMain.java
+++ src/main/java/localvariables/DistanceMain.java
@@ -1,13 +1,16 @@
 package localvariables;
 
 public class DistanceMain {
-    public static void main(String[] args) {
-        Distance distance = new Distance(120.55, true);
-        System.out.println(distance.getDistanceInKm() + " " + distance.isExact());
 
-//ezt meg kellett néznem a megoldókulcsban, de már értem.
-        int distanceWholeNumber = (int)distance.getDistanceInKm();
-                System.out.println(distanceWholeNumber);
+    public static void main(String[] args) {
+
+        Distance distance = new Distance (800.5, false);
+        System.out.println(distance.getDistanceInKm() + ", " + distance.isExact());
+
+        int egeszResz = (int)(distance.getDistanceInKm());
+        System.out.println(egeszResz);
+
+
 
 
 
