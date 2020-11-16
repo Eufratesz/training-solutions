1474a5e79a8e7022bc0be415a16dd79820cf5e5b
Lökösházi Anett <lokoshazi@freemail.hu>
2020-11-16 11:03:24 +0100

    Gyakorlásképp újra megoldottam, az elnevezését javítottam (StatementsMain-ről)

---
+++ src/main/java/statements/StatementMain.java
@@ -0,0 +1,25 @@
+package statements;
+
+public class StatementMain {
+
+    public static void main(String[] args) {
+
+       int x = 5 + 6;
+       int y = 11 - x;
+       int z = 8;
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+
+       boolean b = x > y;
+       boolean c = b == true || z > 5;  //megoldókulcsban: boolean c = b || ( z> 5);
+
+        System.out.println(b);
+        System.out.println(c);
+
+        z++;
+
+        System.out.println(z);
+
+
+    }
+}
