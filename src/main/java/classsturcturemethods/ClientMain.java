651f818c42ee2ff574adda57ed8a5efd6f7d3c80
Lökösházi Anett <lokoshazi@freemail.hu>
2020-11-26 20:28:34 +0100

    Javítottam a nevét (Cliente -> Client

---
+++ src/main/java/classsturcturemethods/ClientMain.java
@@ -0,0 +1,20 @@
+package classsturcturemethods;
+
+public class ClientMain {
+    public static void main(String[] args) {
+        Client client = new Client();
+
+        client.setName("Brad Pitt");
+        client.setYear(1963);
+        client.setAddress("Hollywood");
+
+        System.out.println(client.getName());
+        System.out.println(client.getYear());
+        System.out.println(client.getAddress());
+
+        client.migrate("Paris");
+        System.out.println(client.getAddress());
+
+        System.out.println("Your address has officially been changed!");
+    }
+}
