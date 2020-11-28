45d3f633db816bf91a12dcb601d4ab7e38d0aa1b
Eufratesz <lokoshazi@freemail.hu>
2020-11-28 20:41:58 +0100

    Jó

--- src/main/java/packages/greetings/Greeter.java
+++ src/main/java/packages/greetings/Greeter.java
@@ -3,6 +3,7 @@ package packages.greetings;
 public class Greeter {
 
     public void sayHello() {
+
         System.out.println("Hello World!");
     }
 }
--- src/main/java/packages/main/MainProgram.java
+++ src/main/java/packages/main/MainProgram.java
@@ -6,10 +6,7 @@ public class MainProgram {
 
     public static void main(String[] args) {
         Greeter greeter = new Greeter();
-
         greeter.sayHello();
-
-
 
     }
 }
