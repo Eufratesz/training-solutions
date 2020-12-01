5d201111b51821718be676bba95413c6f2d32b0e
Eufratesz <lokoshazi@freemail.hu>
2020-12-01 16:03:09 +0100

    Konzultáció után már ment egyedül is.

---
+++ src/main/java/week06d02/Store.java
@@ -0,0 +1,28 @@
+package week06d02;
+
+import java.util.List;
+
+public class Store {
+
+    public List<Product> products;
+
+    public Store(List<Product> products) {
+        this.products = products;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public int getProductByCategoryName(String categoryName) {
+        int count = 0;
+        for (Product p : products) {
+            if (p.getCategory().equals(categoryName)) {
+                count++;
+            }
+
+        }
+        return count;
+
+    }
+}
