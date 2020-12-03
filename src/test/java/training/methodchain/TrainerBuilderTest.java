48288c61833384ea9459e99504f7563e15152dbc
Eufratesz <lokoshazi@freemail.hu>
2020-12-03 20:12:51 +0100

    Gyakoroltam

---
+++ src/test/java/training/methodchain/TrainerBuilderTest.java
@@ -0,0 +1,20 @@
+package training.methodchain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TrainerBuilderTest {
+
+    @Test
+    public void build() {
+
+        Trainer trainer = new TrainerBuilder().withName("John Doe").withAge(30).build();
+
+        assertEquals("John Doe", trainer.getName());
+        assertEquals(30, trainer.getAge());
+
+
+
+    }
+}
