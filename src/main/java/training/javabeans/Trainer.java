3a9148404727dcf8ad89192b0f47c8199ff56d5a
Eufratesz <lokoshazi@freemail.hu>
2020-12-05 19:00:39 +0100

    Gyakorlás

---
+++ src/main/java/training/javabeans/Trainer.java
@@ -0,0 +1,23 @@
+package training.javabeans;
+
+public class Trainer {
+
+    private String name;
+    private boolean active;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
