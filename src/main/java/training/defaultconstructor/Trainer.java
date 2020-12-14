f0d07516bd0eb596c75f8cbf125dc28c40071e7d
Eufratesz <lokoshazi@freemail.hu>
2020-12-14 11:44:22 +0100

    Gyakorlás

---
+++ src/main/java/training/defaultconstructor/Trainer.java
@@ -0,0 +1,13 @@
+package training.defaultconstructor;
+
+public class Trainer {
+    private String name;
+
+    public Trainer() {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/test/java/training/defaultconstructor/TrainerTest.java
@@ -0,0 +1,15 @@
+package training.defaultconstructor;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TrainerTest {
+
+    @Test
+    public void testCreate() {
+        Trainer trainer = new Trainer();
+        assertEquals(null, trainer.getName());
+
+    }
+}
