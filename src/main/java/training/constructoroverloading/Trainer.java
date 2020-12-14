4918f88f7319257e7dfe8eaa2f81e30e5d969b8c
Eufratesz <lokoshazi@freemail.hu>
2020-12-14 13:18:13 +0100

    Gyakorlás

---
+++ src/main/java/training/constructoroverloading/Trainer.java
@@ -0,0 +1,29 @@
+package training.constructoroverloading;
+
+public class Trainer {
+
+    private String name;
+    private int age;
+
+    public Trainer() {
+        this("anonym");
+    }
+
+    public Trainer(String name) {
+        this(name, 20);
+    }
+
+    public Trainer(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
---
+++ src/test/java/training/constructoroverloading/TrainerTest.java
@@ -0,0 +1,16 @@
+package training.constructoroverloading;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TrainerTest {
+
+    @Test
+    public void testCreate() {
+        Trainer trainer = new Trainer("John Doe");
+
+        assertEquals("John Doe",trainer.getName());
+        assertEquals(20, trainer.getAge());
+    }
+}
