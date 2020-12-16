60c6d8e071cae4dfa199605fb573671261063d73
Eufratesz <lokoshazi@freemail.hu>
2020-12-16 20:27:04 +0100

    Gyakorlás

---
+++ src/main/java/training/inheritance2constructor/Employee.java
@@ -0,0 +1,10 @@
+package training.inheritance2constructor;
+
+public class Employee extends Person{
+    private int salary;
+
+    public Employee(String name, int age, int salary) {
+        super (name, age);
+        this.salary = salary;
+    }
+}
---
+++ src/main/java/training/inheritance2constructor/Person.java
@@ -0,0 +1,19 @@
+package training.inheritance2constructor;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
