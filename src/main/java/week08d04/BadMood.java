02a24c177c27f692acb8549aa7be8c74afe89656
Eufratesz <lokoshazi@freemail.hu>
2020-12-17 15:44:59 +0100

    Sikerült:)

---
+++ src/main/java/week08d04/BadMood.java
@@ -0,0 +1,11 @@
+package week08d04;
+
+public class BadMood implements CanMark{
+
+    @Override
+    public int giveMark() {
+        return 3;
+    }
+}
+
+
---
+++ src/main/java/week08d04/GoodMood.java
@@ -0,0 +1,14 @@
+package week08d04;
+
+public class GoodMood implements CanMark {
+
+    private int mark;
+
+    @Override
+    public int giveMark() {
+        return 5;
+    }
+
+
+
+}
