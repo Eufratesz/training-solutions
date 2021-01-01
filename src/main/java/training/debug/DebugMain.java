3c6110a8e97558ffab3b03181b0fafa4536aa692
Eufratesz <lokoshazi@freemail.hu>
2021-01-01 17:25:13 +0100

    gyakorlás

---
+++ src/main/java/training/debug/DebugMain.java
@@ -0,0 +1,23 @@
+package training.debug;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DebugMain {
+
+    public int find(List<String>words, String prefix) {
+        int count = 0;
+        for(String word : words){
+            if(word.startsWith(prefix)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    public static void main(String[] args) {
+        int count = new DebugMain().find(Arrays.asList("One", "Two", "Three", "Onehundred"), "One");
+        System.out.println(count);
+    }
+}
