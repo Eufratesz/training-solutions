ca77f4333830498f8331ee5059bc21476787507c
Eufratesz <lokoshazi@freemail.hu>
2021-01-01 11:15:46 +0100

    gyakorlás

---
+++ src/main/java/training/arrayofarrays/ArrayOfArraysMain.java
@@ -0,0 +1,26 @@
+package training.arrayofarrays;
+
+public class ArrayOfArraysMain {
+
+    public static void main(String[] args) {
+        /*
+        int [][] c = new int [3][3];
+        int [][] b = new int [3][6];
+
+        int [][] a = new int [3][];
+        a[0] = new int[1];
+        a[1] = new int[2];
+        a[2] = new int[3];
+
+        a[0][0] = 8;
+
+
+         */
+        int[][] a = {{1}, {1, 2}, {1, 2, 3}};
+
+
+
+
+    }
+}
+
