f715cbbb8c71dfbb49c950df36a12098fb6b4ed5
Eufratesz <lokoshazi@freemail.hu>
2021-01-01 15:38:05 +0100

    egyedül sikerült

---
+++ src/main/java/arraylist/Capsules.java
@@ -0,0 +1,56 @@
+package arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Capsules {
+
+    private List<String> colours = new ArrayList<>();
+
+    public void addLast(String colour) {
+        colours.add(colour);
+    }
+
+    public void addFirst(String colour) {
+        colours.add(0, colour);
+    }
+
+    public void removeFirst() {
+        colours.remove(0);
+    }
+
+    public void removeLast() {
+        colours.remove(colours.size()-1);
+    }
+
+    public List<String> getColours() {
+        return colours;
+    }
+
+    public static void main(String[] args) {
+
+       Capsules capsules = new Capsules();
+       capsules.addLast("piros");
+       capsules.addLast("kék");
+       capsules.addLast("zöld");
+       capsules.addLast("sárga");
+       System.out.println(capsules.getColours());
+
+       capsules.addFirst("barna");
+       System.out.println(capsules.getColours());
+
+       capsules.removeLast();
+        System.out.println(capsules.getColours());
+
+        capsules.removeFirst();
+        System.out.println(capsules.getColours());
+
+
+
+
+
+
+
+
+    }
+}
