704e32286637bac94bdccb3633d1855d3a20f08b
Eufratesz <lokoshazi@freemail.hu>
2021-01-02 09:28:34 +0100

    ismétlés

---
+++ src/main/java/training/finalmodifier/Circle.java
@@ -0,0 +1,17 @@
+package training.finalmodifier;
+
+public class Circle {
+
+    public static final double PI = 3.14;
+
+    private final double r;
+
+    public Circle(double r) {
+        this.r = r;
+    }
+
+    public double calculatePerimeter(double r) {
+
+        return 2*r*PI;
+    }
+}
---
+++ src/main/java/training/finalmodifier/CircleMain.java
@@ -0,0 +1,11 @@
+package training.finalmodifier;
+
+public class CircleMain {
+
+    public static void main(String[] args) {
+        System.out.println(Circle.PI);
+
+
+        System.out.println(new Circle(5.0).calculatePerimeter(5.0));
+    }
+}
