a8676a3beab95dfa530b003e7902fb29ce5504e1
Eufratesz <lokoshazi@freemail.hu>
2021-01-03 07:56:04 +0100

    ismétlés

---
+++ src/main/java/training/literals/LiteralsMain.java
@@ -0,0 +1,18 @@
+package training.literals;
+
+public class LiteralsMain {
+    public static void main(String[] args) {
+        System.out.println(0b1111_1111);
+        System.out.println(0377);
+        System.out.println(0xff);
+
+        long l =100_000_000_000L;
+
+        System.out.println(-12e4);
+
+        float f =1.0F;
+
+        System.out.println("Hello \n\"World\"!");
+
+    }
+}
