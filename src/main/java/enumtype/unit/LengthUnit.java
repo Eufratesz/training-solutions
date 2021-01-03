261159823057f9b924a95a781a728605f2284d21
Eufratesz <lokoshazi@freemail.hu>
2021-01-03 14:14:05 +0100

    nagyrészt megoldókulcs alapján

---
+++ src/main/java/enumtype/unit/LengthUnit.java
@@ -0,0 +1,27 @@
+package enumtype.unit;
+
+public enum LengthUnit {
+
+    MILLIMETER(1, true),
+    CENTIMETER(10, true),
+    METER(100, true),
+    YARD(9144, false),
+    FOOT(3048, false),
+    INCH(254, false);
+
+    private final int inMillimeter;
+    private final boolean siUnit;
+
+    LengthUnit(int inMillimeter, boolean siUnit) {
+        this.inMillimeter = inMillimeter;
+        this.siUnit = siUnit;
+    }
+
+    public int getInMillimeter() {
+        return inMillimeter;
+    }
+
+    public boolean isSiUnit() {
+        return siUnit;
+    }
+}
