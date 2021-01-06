c24d13e298d6087c4024546a5ed441ecff8fbc2c
Eufratesz <lokoshazi@freemail.hu>
2021-01-06 18:38:40 +0100

    lemaradt:(

--- src/test/java/training/methodchain/TrainerTest.java
+++ src/test/java/training/methodchain/TrainerTest.java
@@ -7,8 +7,10 @@ public class TrainerTest {
     @Test
     public void testCreate() {
         Trainer trainer = new Trainer().setName("John Doe").setAge(30);
-        trainer.setName("John Doe");
-        trainer.setAge(30);
+
+
+        //trainer.setName("John Doe");
+       // trainer.setAge(30);
     }
     Ezt nem nem nagyon használjuk, mert a javabeans-nek nem felel meg.
  */
