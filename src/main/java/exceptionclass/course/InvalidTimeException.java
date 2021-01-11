47670be498a392a08f3ed8d4c71580aca19945e7
Eufratesz <lokoshazi@freemail.hu>
2021-01-11 07:41:34 +0100

    nehéz volt

---
+++ src/main/java/exceptionclass/course/InvalidTimeException.java
@@ -0,0 +1,13 @@
+package exceptionclass.course;
+
+public class InvalidTimeException extends RuntimeException{
+
+   public InvalidTimeException() {
+
+   }
+
+   public InvalidTimeException(String message) {
+       super(message);
+   }
+
+}
