96e2c9853839f0f3fa50199f336c718b482e32bb
Eufratesz <lokoshazi@freemail.hu>
2021-01-21 12:29:54 +0100

    gyakorlás

---
+++ src/main/java/training/iowriter/EmployeeService.java
@@ -0,0 +1,27 @@
+package training.iowriter;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class EmployeeService {
+
+    public static void main(String[] args) {
+        Path file = Path.of("src/main/java/training/iowriter/employees.txt");  //sikerült ide is:)
+
+        List<String> employees = List.of("John Doe", "Jane Doe", "Jack Doe");
+
+        try (BufferedWriter writer =Files.newBufferedWriter(file)) {
+            for(String employee: employees) {
+                writer.write(employee);
+                writer.write("\n");
+            }
+        }
+            catch (IOException ioe) {
+            throw new IllegalStateException("Cannot write file");
+
+        }
+    }
+}
