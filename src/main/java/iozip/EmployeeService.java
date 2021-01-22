99edcd8da78626bf40a2919fe5587ed62323c588
Eufratesz <lokoshazi@freemail.hu>
2021-01-22 15:09:17 +0100

    gyakorlás

---
+++ src/main/java/iozip/EmployeeService.java
@@ -0,0 +1,25 @@
+package iozip;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class EmployeeService {
+
+    public static void main(String[] args) {
+        Path path = Path.of("src/main/resources/dataiozip.zip");
+        try(ZipOutputStream os = new ZipOutputStream(new BufferedOutputStream((Files.newOutputStream(path))))) {
+            os.putNextEntry(new ZipEntry("dataiozip.dat"));
+            for (int i=0; i < 1100; i++) {
+                os.write("abcde".getBytes());
+            }
+        }
+        catch (IOException ioe) {
+            throw new IllegalStateException("Cannot write file");
+        }
+    }
+}
