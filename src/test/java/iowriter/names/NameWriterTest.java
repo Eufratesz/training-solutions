c8c74a68ffcd85e48472f306edce7d8fd50b887c
Eufratesz <lokoshazi@freemail.hu>
2021-01-25 08:14:11 +0100

    értem

--- src/test/java/iowriter/names/NameWriterTest.java
+++ src/test/java/iowriter/names/NameWriterTest.java
@@ -3,7 +3,6 @@ package iowriter.names;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
