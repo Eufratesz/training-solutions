79943830eb1daa5042e4ff74d0120fa1245e342c
Eufratesz <lokoshazi@freemail.hu>
2021-02-02 10:18:29 +0100

    Sikerült:)

---
+++ src/test/java/collectionsiterator/LibraryManagerTest.java
@@ -0,0 +1,75 @@
+package collectionsiterator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LibraryManagerTest {
+
+   private LibraryManager manager;
+
+    @BeforeEach
+    public void bookSetShoudBeCreated(){
+        Set<Book>libraryBooks = new HashSet<>(Arrays.asList(new Book(12345, "Animal Farm", "George Orwell"),
+                new Book(67890, "And there were none", "Agatha Christie"),
+                new Book(28374, "1984", "George Orwell")));
+        manager = new LibraryManager(libraryBooks);
+
+    }
+
+    @Test
+    void missingRegnumberShouldThrowException()throws MissingBookException{
+        Exception ex = assertThrows(MissingBookException.class, ()->{
+            manager.findBookByRegnumber(12346);
+        });
+        assertEquals("No books found with regnumber: 12346", ex.getMessage());
+    }
+
+    @Test
+    void TestFindBookByRegNumber(){
+        //When
+        Book book = manager.findBookByRegnumber(67890);
+        //Then
+        assertEquals("And there were none", book.getTitle());
+        assertEquals("Agatha Christie", book.getAuthor());
+    }
+
+    @Test
+    void noRegNumberAtRemoveShouldThrowException() throws MissingBookException {
+        Exception ex = assertThrows(MissingBookException.class,()-> {
+            manager.removeBookByRegnumber(67891);
+        });
+        assertEquals("No books found with regnumber: 67891", ex.getMessage());
+    }
+
+    @Test
+     void testRemoveByRegnumber(){
+        //When
+        int foundBook = manager.removeBookByRegnumber(67890);
+        //Then
+        assertEquals(67890, foundBook);
+        assertEquals(2, manager.libraryBooksount());
+    }
+
+    @Test
+    void noSuchAuthorShouldThrowException() throws MissingBookException{
+        Exception ex = assertThrows(MissingBookException.class,()->{
+            manager.selectBooksByAuthor("Benedek Elek");
+        });
+        assertEquals("No books found written by: Benedek Elek", ex.getMessage());
+    }
+
+    @Test
+    void testSelectBooksByAuthor() {
+        //When
+        Set<Book>foundBooks = manager.selectBooksByAuthor("George Orwell");
+        //Then
+        assertEquals(2, foundBooks.size());
+    }
+
+}
