578ff9aad177423144fd5bae82b9c835f5cfb1c0
Eufratesz <lokoshazi@freemail.hu>
2021-02-10 15:49:37 +0100

    teszteset folytatása

--- src/test/java/week15d03/PostFinderTest.java
+++ src/test/java/week15d03/PostFinderTest.java
@@ -14,11 +14,19 @@ class PostFinderTest {
         PostFinder postFinder = new PostFinder(Arrays.asList(
                 new Post(null, LocalDate.of(2020,1,1), "Content1", "John Doe")));
         assertThrows(IllegalArgumentException.class,()-> new PostFinder(Arrays.asList()).findPostsFor("John Doe"));
-             //   assertEquals("Title or content must not be empty", ex.getMessage());
+          //    assertEquals("Title or content must not be empty", ex.getMessage());
     }
 
     @Test
     void TestFindPostsFor() {
+        PostFinder postFinder = new PostFinder(Arrays.asList(
+                new Post("Title1", LocalDate.of(2020,1,1), "Content1", "John Doe")));
+        assertEquals("Title1", postFinder.findPostsFor("John Doe").get(0).getTitle());
+        assertEquals(LocalDate.of(2020,1,1), postFinder.findPostsFor("John Doe").get(0).getPublishedAt());
+        assertEquals("Content1", postFinder.findPostsFor("John Doe").get(0).getContent());
+        assertEquals("John Doe", postFinder.findPostsFor("John Doe").get(0).getOwner());
+
+
 
     }
 }
