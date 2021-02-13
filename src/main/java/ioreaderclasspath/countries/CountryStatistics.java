0c7a632d742a6b96e652918a3cd9e860d0ba2147
Eufratesz <lokoshazi@freemail.hu>
2021-02-13 16:19:13 +0100

    ismétlés

---
+++ src/main/java/ioreaderclasspath/countries/CountryStatistics.java
@@ -0,0 +1,50 @@
+package ioreaderclasspath.countries;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CountryStatistics {
+
+    private List<Country> countries = new ArrayList<>();
+
+    public void readFromFile(String fileName) {
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(CountryStatistics.class.getResourceAsStream("/" + fileName)))) {
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                String[] temp = line.split(" ");
+                Country country = new Country(temp[0], Integer.parseInt(temp[1]));
+                countries.add(country);
+            }
+
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Cannot read file", ioe);
+        }
+
+    }
+
+    public int numberOfCountries() {
+        return countries.size();
+    }
+
+    public Country maxNeighbours() {
+        Country max = countries.get(0);
+        for (Country country : countries) {
+            if (country.getNeighbours() > max.getNeighbours()) {
+                max = country;
+            }
+        }
+        return max;
+    }
+
+    public List<Country> getCountries() {
+        return new ArrayList<>(countries);
+    }
+}
+
+
+
---
+++ src/test/java/ioreaderclasspath/countries/CountryTest.java
@@ -0,0 +1,17 @@
+package ioreaderclasspath.countries;
+
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CountryTest {
+    private Country country = new Country("Spain", 3);
+
+    @Test
+    public void countryShouldCreate(){
+        assertEquals("Spain", country.getName());
+        assertEquals(3, country.getNeighbours());
+    }
+
+}
