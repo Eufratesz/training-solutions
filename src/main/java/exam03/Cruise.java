659d179318f10e173bce7f787bc9250e1823ba71
Eufratesz <lokoshazi@freemail.hu>
2021-02-22 10:56:02 +0100

    ismétlés

--- src/main/java/exam03/Cruise.java
+++ src/main/java/exam03/Cruise.java
@@ -1,23 +1,17 @@
+
 package exam03;
 
+
+import java.text.Collator;
 import java.time.LocalDate;
 import java.util.*;
 
 public class Cruise {
 
-
     private Boat boat;
     private LocalDate sailing;
     private double basicPrice;
-    private List<Passenger> passengers;
-
-
-    public Cruise(Boat boat, LocalDate sailing, double basicPrice, List<Passenger> passengers) {
-        this.boat = boat;
-        this.sailing = sailing;
-        this.basicPrice = basicPrice;
-        this.passengers = passengers;
-    }
+    private List<Passenger> passengers = new ArrayList<>();
 
     public Cruise(Boat boat, LocalDate sailing, double basicPrice) {
         this.boat = boat;
@@ -25,70 +19,89 @@ public class Cruise {
         this.basicPrice = basicPrice;
     }
 
-    public boolean bookPassenger(Passenger passenger) {
-        for (int i = 0; i < passengers.size(); i++) {
-            if (i < boat.getMaxPassengers()) {
-                passengers.add(passenger);
-            }
+    public void bookPassenger(Passenger passenger) {
+        if(boat.getMaxPassengers() <= passengers.size()){
+            throw new IllegalArgumentException("Too many passengers.");
         }
-        return true;
+        passengers.add(passenger);
     }
 
-
-    public double getPriceForPassenger(Passenger passenger) {
-        double price = passenger.getCruiseClass().getValue();
-        return price;
-
+    public double getPriceForPassenger(Passenger passenger){
+        if (passenger.getCruiseClass() == CruiseClass.LUXURY) {
+            return basicPrice *3;
+        }
+        else if (passenger.getCruiseClass() == CruiseClass.FIRST) {
+            return basicPrice *1.8;
+        }
+        else if (passenger.getCruiseClass() == CruiseClass.SECOND){
+            return basicPrice;
+        }
+        else {
+            throw new IllegalArgumentException("No price for class");
+        }
     }
 
-    public Passenger findPassengerByName(String name) {
+    public Passenger findPassengerByName(String name){
 
-        for (int i = 0; i < passengers.size(); i++) {
-            if (passengers.contains(name)) {
-                Passenger foundPassenger =passengers.get(i);
-                return foundPassenger;
+        for(Passenger passenger: passengers){
+            if(passenger.getName().equals(name)){
+                return passenger;
+
             }
         }
-        return ;
+        throw new IllegalArgumentException("Cannot find the name: " + name);
     }
 
     public List<String> getPassengerNamesOrdered() {
-        List<String> namesOrdered = new ArrayList<>();
-        for(Passenger p: passengers)
-            namesOrdered = Collections.sort(passengers);
-        return namesOrdered;
+        List<String> orderedList = new ArrayList<>();
 
+        for(Passenger passenger : passengers){
+            orderedList.add(passenger.getName());
+
+        }
+        Collections.sort(orderedList, Collator.getInstance(new Locale("hu", "Hu")));
+        return orderedList;
     }
 
-    public double sumAllBookingsCharged() {
-        double sum=0;
-        double price = getPriceForPassenger();
-        for (int price  ) {
-            sum += price;
+    public double sumAllBookingsCharged(){
+        double sum = 0.0;
+        for(Passenger passenger : passengers) {
+            sum+= getPriceForPassenger(passenger);
         }
         return sum;
-
     }
 
-    Map<CruiseClass, Integer> countPassengerByClass() {
-        Map<CruiseClass, Integer>booking = new HashMap<>();
-        for()
+    public Map<CruiseClass, Integer> countPassengerByClass(){
+        Map<CruiseClass, Integer> counts = new HashMap<>();
+        for(Passenger passenger :passengers) {
+            if(!counts.containsKey(passenger.getCruiseClass())) {
+                counts.put(passenger.getCruiseClass(), 1);
+            }
+            else {
+                counts.put(passenger.getCruiseClass(), counts.get(passenger.getCruiseClass())+1);
+
+            }
+        }
+        return counts;
     }
 
-        public Boat getBoat () {
-            return boat;
-        }
 
-        public LocalDate getSailing () {
-            return sailing;
-        }
 
-        public double getBasicPrice () {
-            return basicPrice;
-        }
 
-        public List<Passenger> getPassengers () {
-            return passengers;
-        }
+    public Boat getBoat() {
+        return boat;
+    }
+
+    public LocalDate getSailing() {
+        return sailing;
+    }
+
+    public double getBasicPrice() {
+        return basicPrice;
+    }
+
+    public List<Passenger> getPassengers() {
+        return passengers;
     }
 }
+
