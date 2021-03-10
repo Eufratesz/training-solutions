30824bc0458c39c783cf02f617f222a33b439ce6
Eufratesz <lokoshazi@freemail.hu>
2021-03-10 16:22:50 +0100

    ismétlés

---
+++ src/main/java/extrapractice/packages/main/MainProgram.java
@@ -0,0 +1,13 @@
+package extrapractice.packages.main;
+
+import extrapractice.packages.greetings.Greeter;
+
+public class MainProgram {
+
+    public static void main(String[] args) {
+
+        new Greeter().sayHello();
+
+
+    }
+}
