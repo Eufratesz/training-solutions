109b03f44f15825dc74ba9646879368bb4ec9648
Eufratesz <lokoshazi@freemail.hu>
2021-03-12 17:14:47 +0100

    ismétlés

--- src/main/java/extrapractice/array/ArrayOfArrayMain.java
+++ src/main/java/extrapractice/array/ArrayOfArrayMain.java
@@ -2,11 +2,15 @@ package extrapractice.array;
 
 public class ArrayOfArrayMain {
 
-    public int[][]multiplicationTable(int size){
+
+
+
+
+    public int[][] multiplicationTable(int size) {
         int[][] table = new int[size][size];
-        for(int i =0; i< size; i++){
-            for (int j=0; i< size; i++){
-                table[i][j] = (i+1)* (j+1);
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; i < size; i++) {
+                table[i][j] = (i + 1) * (j + 1);
             }
         }
         return table;
@@ -14,4 +18,28 @@ public class ArrayOfArrayMain {
     }
 
 
-}
+
+
+        public void printArrayOfArrays(int[][] a){
+            for (int i[] : a) {
+                for (int j : i) {
+                    if (j < 10) {
+                        System.out.print("  ");
+                    } else if (j < 100) {
+                        System.out.print(" ");
+                    }
+                    System.out.print(j);
+                    System.out.print(" ");
+                }
+                System.out.println();
+            }
+        }
+
+    public static void main(String[] args) {
+        ArrayOfArrayMain am = new ArrayOfArrayMain();
+
+        am.printArrayOfArrays(am.multiplicationTable(4));
+    }
+
+
+    }
