d3ba9f59e96e40d5c3ee57c3da6e621ae6167846
Eufratesz <lokoshazi@freemail.hu>
2021-03-12 16:27:12 +0100

    ismétlés

---
+++ src/main/java/extrapractice/array/ArrayHandler.java
@@ -0,0 +1,23 @@
+package extrapractice.array;
+
+public class ArrayHandler {
+
+    public boolean contains(int[] source, int itemToFind){
+        for(int item : source){
+            if (item == itemToFind) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public int find(int[] source, int itemToFind){
+        for(int i= 0; i< source.length; i++){
+            if (source[i] == itemToFind) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
