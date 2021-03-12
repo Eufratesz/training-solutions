861b007bccf11c04e59973374d668f32bf3d25ee
Eufratesz <lokoshazi@freemail.hu>
2021-03-12 15:14:22 +0100

    ismétlés

--- src/main/java/training/array/ArrayMain.java
+++ src/main/java/training/array/ArrayMain.java
@@ -3,25 +3,25 @@ package training.array;
 public class ArrayMain {
 
     public static void main(String[] args) {
-        int[] a = new int[3];
-        a[0] = 1;
-        System.out.println(a[0]);
-        System.out.println(a[1]);
-
-        System.out.println(a.length);
-
-
-
-        int[]b = {1, 2, 3};
-
-        for (int i=0; i < b.length; i++) {
-            System.out.println(b[i]);
-        }
-//for each cycle:
-        for (int item: b) {
-            System.out.println(item);
-        }
-
+//        int[] a = new int[3];
+//        a[0] = 1;
+//        System.out.println(a[0]);
+//        System.out.println(a[1]);
+//
+//        System.out.println(a.length);
+//
+//
+//
+//        int[]b = {1, 2, 3};
+//
+//        for (int i=0; i < b.length; i++) {
+//            System.out.println(b[i]);
+//        }
+////for each loop:
+//        for (int item: b) {
+//            System.out.println(item);
+//        }
+//
 
 
         String[] fruits = {"apple", "peach", "plum", "orange"};
@@ -32,6 +32,10 @@ public class ArrayMain {
         String[] favouriteFruits = new String[2];
         System.arraycopy(fruits, 1, favouriteFruits, 0, 2);
         System.out.println(favouriteFruits);
+
+        String[] myFavourites = new String [4];
+        System.arraycopy(fruits, 2, myFavourites, 0, 3);
+        System.out.println(myFavourites);
 
 
 
