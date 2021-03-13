ecc6dfd80c5a17417aa8f249bd6b44a21f96c634
Eufratesz <lokoshazi@freemail.hu>
2021-03-13 16:13:30 +0100

    ismétlés

---
+++ src/main/java/extrapractice/math/MathMain.java
@@ -0,0 +1,18 @@
+package extrapractice.math;
+
+import java.util.Random;
+
+public class MathMain {
+
+    public static void main(String[] args) {
+
+        System.out.println(Math.PI);
+        System.out.println(Math.round(10.5));
+        System.out.println(Math.abs(-10));
+        System.out.println(Math.max(-10, 5));
+        System.out.println(Math.min(-10, 5));
+
+
+        System.out.println(new Random(13).nextInt(100));
+    }
+}
