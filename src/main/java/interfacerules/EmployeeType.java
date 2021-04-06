3d3a29f8513719eadfa8e615ed2e3ead8ecb842d
Eufratesz <lokoshazi@freemail.hu>
2021-04-06 10:14:18 +0200

    ismétlés

---
+++ src/main/java/interfacerules/EmployeeType.java
@@ -0,0 +1,7 @@
+package interfacerules;
+
+public interface EmployeeType {
+
+    int FULL_TIME = 0;
+    int PART_TIME = 1;
+}
---
+++ src/main/java/interfacerules/TrainerMain.java
@@ -0,0 +1,14 @@
+package interfacerules;
+
+public class TrainerMain {
+
+    public static void main(String[] args) {
+        HasName hasName = new Trainer();
+        hasName.getName();
+
+        CanWork canWork = new Trainer();
+        canWork.doWork();
+
+        System.out.println(EmployeeType.FULL_TIME);
+    }
+}
